c12d8b2d68514ebbc0b40e4f4b416da901e065ab
Ingemar Bergmark <ingemar@swipeware.com>
2014-09-14 14:20:28 +0900

    Corona sample project update

--- Corona/main.lua
+++ Corona/main.lua
@@ -4,6 +4,8 @@ The MIT License (MIT)
 
 Copyright (c) 2014 Gremlin Interactive Limited
 
+Updated for Chartboost SDK 5.x by Ingemar Bergmark, Swipeware (www.swipeware.com)
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -24,105 +26,94 @@ THE SOFTWARE.
 // ----------------------------------------------------------------------------
 --]]
 
-local chartboost = require( "plugin.chartboost" );
-local widget = require( "widget" );
-widget.setTheme( "widget_theme_ios" );
+local chartboost = require( "plugin.chartboost" )
+local widget = require( "widget" )
+widget.setTheme( "widget_theme_ios" )
 
 -- The ChartBoost listener function
 local function chartBoostListener( event )
-	for k, v in pairs( event ) do
-		print( k, ":", v )
-	end
+    for k, v in pairs( event ) do
+        print( k, ":", v )
+    end
 end
 
 -- Your ChartBoost app id
-local yourAppID = "";
+local yourAppID = ""
 -- Your ChartBoost app signature
-local yourAppSignature = "";
+local yourAppSignature = ""
 
 -- Change the appid/sig for android (required by Chartboost)
 if system.getInfo( "platformName" ) == "Android" then
-	yourAppID = "";
-	yourAppSignature = "";
+    yourAppID = ""
+    yourAppSignature = ""
 end
 
-print( "plugin version is: ", chartboost.getPluginVersion() );
+print( "plugin version is: ", chartboost.getPluginVersion() )
 
 -- Initialise ChartBoost
-chartboost.init(
-	{
-		--licenseKey = "PAS0H-STWVI-WXVUK-2RP4P",
-		appID = yourAppID,
-		appSignature = yourAppSignature, 
-		listener = chartBoostListener,
-		--betaMode = true,
-	}
-);
-
---chartboost.show( "interstitial" )
---chartboost.cache( "bah" );
+chartboost.init {
+    appID        = yourAppID,
+    appSignature = yourAppSignature, 
+    listener     = chartBoostListener
+}
 
 -- Show Ad button
 local showAdButton = widget.newButton
 {
-	label = "Show Ad";
-	onRelease = function( event )
-		print( "has cached Ad?:", chartboost.hasCachedInterstitial( "myLoc" ) );
-		print( "has cached more apps?:", chartboost.hasCachedMoreApps() );
-	
-		if chartboost.hasCachedInterstitial( "myLoc" ) then
-			chartboost.show( "interstitial", "myLoc" )
-		else
-			chartboost.show( "interstitial" );
-		end
-	end,
+    label = "Show Ad";
+    onRelease = function( event )
+        print( "has cached Ad?:", chartboost.hasCachedInterstitial() )
+        print( "has cached more apps?:", chartboost.hasCachedMoreApps() )
+    
+        chartboost.show( "interstitial" )
+    end
 }
-showAdButton.x = display.contentCenterX;
-showAdButton.y = 150;
+showAdButton.x = display.contentCenterX
+showAdButton.y = 150
 
 -- Cache Default Ad
 local cacheDefaultAd = widget.newButton
 {
-	label = "Cache Default Ad",
-	onRelease = function( event )
-		chartboost.cache( 'myLoc' );
-	end,
+    label = "Cache Ad",
+    onRelease = function( event )
+        chartboost.cache( "interstitial" )
+    end
 }
-cacheDefaultAd.x = display.contentCenterX;
-cacheDefaultAd.y = showAdButton.y + showAdButton.contentHeight + cacheDefaultAd.contentHeight * 0.5;
+cacheDefaultAd.x = display.contentCenterX
+cacheDefaultAd.y = showAdButton.y + showAdButton.contentHeight + cacheDefaultAd.contentHeight * 0.5
 
 -- Cache More Apps
 local cacheMoreAppsButton = widget.newButton
 {
-	label = "Cache More Apps",
-	onRelease = function( event )
-		chartboost.cache( "moreApps" );
-	end,
+    label = "Cache More Apps",
+    onRelease = function( event )
+        chartboost.cache( "moreApps" )
+    end
 }
-cacheMoreAppsButton.x = display.contentCenterX;
-cacheMoreAppsButton.y = cacheDefaultAd.y + cacheDefaultAd.contentHeight + cacheMoreAppsButton.contentHeight * 0.5;
+cacheMoreAppsButton.x = display.contentCenterX
+cacheMoreAppsButton.y = cacheDefaultAd.y + cacheDefaultAd.contentHeight + cacheMoreAppsButton.contentHeight * 0.5
 
 -- Show More Apps button
 local showMoreAppsButton = widget.newButton
 {
-	label = "Show More Apps";
-	onRelease = function( event )
-		chartboost.show( "moreApps" );
-	end,
+    label = "Show More Apps"
+    onRelease = function( event )
+        chartboost.show( "moreApps" )
+    end
 }
-showMoreAppsButton.x = display.contentCenterX;
-showMoreAppsButton.y = cacheMoreAppsButton.y + cacheMoreAppsButton.contentHeight + showMoreAppsButton.contentHeight * 0.5;
+showMoreAppsButton.x = display.contentCenterX
+showMoreAppsButton.y = cacheMoreAppsButton.y + cacheMoreAppsButton.contentHeight + showMoreAppsButton.contentHeight * 0.5
 
 
 local function systemEvent( event )
-	local phase = event.phase;
+    local phase = event.phase
 
-	if event.type == 'applicationResume' then
-	    -- Start a ChartBoost session
-		chartboost.startSession( yourAppID, yourAppSignature );
-	end
-	
-	return true
+    if event.type == 'applicationResume' then
+        -- Start a ChartBoost session
+        chartboost.startSession( yourAppID, yourAppSignature )
+    end
+    
+    return true
 end
 
-Runtime:addEventListener( 'system', systemEvent );
+Runtime:addEventListener( 'system', systemEvent )
